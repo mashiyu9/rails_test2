d1fd08635cda605128ee177fbf09cac3653c24af
yu shimamura <mashiyu0914@gmail.com>
2019-11-12 01:11:34 +0900

    add:一覧ページの雛形の作成
    とりあえずリンクの作成ちゃんと飛べるように

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,2 +1,18 @@
 class ChatsController < ApplicationController
+  def index
+    @chats = Chat.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def new
+    @chat = Chat.new
+  end
 end
