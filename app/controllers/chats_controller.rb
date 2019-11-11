6293df844af0c30cb25905d44faf048ecdaa6991
yu shimamura <mashiyu0914@gmail.com>
2019-11-12 01:33:05 +0900

    add:new.html.erbの雛形の作成
    新規投稿の機能のβ版実装

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,6 +1,17 @@
 class ChatsController < ApplicationController
+
   def index
     @chats = Chat.all
+  end
+
+  def create
+    # binding.pry
+    @chat = Chat.new(chat_params)
+    if @chat.save
+      redirect_to chats_path, notice: "つぶやきました"
+    else
+      render "new"
+    end
   end
 
   def show
@@ -15,4 +26,10 @@ class ChatsController < ApplicationController
   def new
     @chat = Chat.new
   end
+
+  private
+  def chat_params
+    params.require(:chat).permit(:content)
+  end
+
 end
