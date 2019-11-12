3545f8d4ce25af48dd3d91df06ad014c177be60d
yu shimamura <mashiyu0914@gmail.com>
2019-11-12 17:32:16 +0900

    update: 画面幅を小さくした時の修正
    最低限のレイアウト

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,6 @@ Bundler.require(*Rails.groups)
 module MyFirstApp
   class Application < Rails::Application
 
-    config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
