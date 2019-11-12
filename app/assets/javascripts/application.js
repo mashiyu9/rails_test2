c6301a4834980bb46cc54377831eb6eba1057c61
yu shimamura <mashiyu0914@gmail.com>
2019-11-12 15:14:32 +0900

    fix:javascriptの反映
    コードの記述順のせいでうまくjavascriptが動かなかったのものを修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,12 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery
+//= require jquery_ujs
 //= require_tree .
+
+
+// $(document).ready(function() {
+//   $(".test").css("color","red")
+//   console.log('hello')
+// });
